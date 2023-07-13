73fc0c983edf8f09fd67249ca9552adca846ed6d
Anderson Affonso Bonde <andersonbonde@gmail.com>
2023-07-13 15:15:06 -0300

    Add functionality to place markers in the board

--- script.js
+++ script.js
@@ -9,8 +9,48 @@ const Player = (token) => {
 const gameBoard = (() => {
     let board = [];
 
-    for(let i = 0; i < 3; i++) {
-        board[i] = new Array(3).fill(null);
+    (function buildBoard() {
+        let table = document.querySelector("table");
+        
+        for(let i = 0; i < 3; i++) {
+            board[i] = new Array(3).fill(null);
+
+            let tableRow = document.createElement("tr");
+            table.appendChild(tableRow);
+
+            for(let j = 0; j < 3; j++) {
+                let tableData = document.createElement("td");
+                tableData.dataset.x = i;
+                tableData.dataset.y = j;
+
+                appendListener(tableData);
+
+                tableRow.appendChild(tableData);
+            }
+        }
+    })();
+
+    function appendListener(target) {
+        target.addEventListener("click", placeMarker);
+    }
+
+    function placeMarker(event) {
+        let data = event.target.dataset;
+        let player = game.getPlayer();
+
+        if(isEmpty(data.x, data.y, player)) {
+            event.target.textContent = player.getToken();
+            game.incrementRound();
+        } else {
+            console.log("Place already taken");
+        }
+    }
+
+    function isEmpty(x, y, player) {
+        if(board[x][y] == null) {
+            board[x][y] = player.getToken();
+            return true;
+        }
     }
 
     return {
@@ -18,3 +58,21 @@ const gameBoard = (() => {
     }
 })();
 
+const game = (() => {
+    let round = 1;
+
+    const player1 = Player("X");
+    const player2 = Player("O");
+
+    const getPlayer = () => {
+        return (round % 2) ? player1 : player2; 
+    };
+    const getRound = () => round;
+    const incrementRound = () => round++;
+
+    return {
+        getPlayer,
+        getRound,
+        incrementRound
+    }
+})();
