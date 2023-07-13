f3d8fbeecf914bd4e9bccf02f47bba117bebecbc
Anderson Affonso Bonde <andersonbonde@gmail.com>
2023-07-13 15:52:58 -0300

    Add logic to find winner

--- script.js
+++ script.js
@@ -68,7 +68,35 @@ const game = (() => {
         return (round % 2) ? player1 : player2; 
     };
     const getRound = () => round;
-    const incrementRound = () => round++;
+    const incrementRound = () => {
+        round++;
+        checkWinner();
+    }
+
+    function checkWinner() {
+        let board = gameBoard.board;
+
+        let firstRow = board[0];
+        let secondRow = board[1];
+        let thirdRow = board[2];
+
+        let firstColumn = [board[0][0], board[1][0], board[2][0]];
+        let secondColumn = [board[0][1], board[1][1], board[2][1]];
+        let thirdColumn = [board[0][2], board[1][2], board[2][2]];
+
+        let decrease = [board[0][0], board[1][1], board[2][2]];
+        let increase = [board[2][0], board[1][1], board[0][2]];
+
+        let victoryCondition = [firstRow, secondRow, thirdRow, firstColumn, secondColumn, thirdColumn, decrease, increase];
+
+        victoryCondition.forEach(curr => {
+            let firstElement = curr[0];
+            
+            if(curr.every(curr => curr == firstElement && curr != null)) {
+                return true;
+            }
+        })
+    }
 
     return {
         getPlayer,
