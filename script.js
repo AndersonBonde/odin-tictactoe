7cc7ec08bf7eda614702798a9262b17f7ebfa697
Anderson Affonso Bonde <andersonbonde@gmail.com>
2023-07-13 14:05:42 -0300

    Initial gameBoard module and placeholder html

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Start");
+const Player = (token) => {
+    const getToken = () => token;
+
+    return {
+        getToken
+    }
+};
+
+const gameBoard = (() => {
+    let board = [];
+
+    for(let i = 0; i < 3; i++) {
+        board[i] = new Array(3).fill(null);
+    }
+
+    return {
+        board
+    }
+})();
+
